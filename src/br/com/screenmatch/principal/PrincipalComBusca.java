e0449a2cde8539e4615acd00d4867ac3bee2586d
Matheus Bodo <matheusbodo167@gmail.com>
2025-03-20 00:10:50 -0300

    fixing API error

--- src/br/com/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/screenmatch/principal/PrincipalComBusca.java
@@ -8,6 +8,7 @@ import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -19,32 +20,34 @@ public class PrincipalComBusca {
         System.out.println("Digite um filme para a busca: ");
         var busca = leitura.nextLine();
 
-        String endereco = "http://www.omdbapi.com/?i=" + busca + "&apikey=1ed12d1f";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();;
-        TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
-        System.out.println(meuTituloOMDB);
+        String tituloCodificado = URLEncoder.encode(busca, "UTF-8");
+        String endereco = "http://www.omdbapi.com/?t=" + tituloCodificado + "&apikey=1ed12d1f";
 
         try {
-        Titulo meuTitulo = new Titulo(meuTituloOMDB);
-        System.out.println("Titulo ja convertido: ");
-        System.out.println(meuTitulo);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();;
+            TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
+            System.out.println(meuTituloOMDB);
+
+
+            Titulo meuTitulo = new Titulo(meuTituloOMDB);
+            System.out.println("Titulo ja convertido: ");
+            System.out.println(meuTitulo);
         } catch (NumberFormatException e){
             System.out.println("Erro: ");
             System.out.println(e.getMessage());
         }
-
+        System.out.println("Finalizado com sucesso");
     }
 }
